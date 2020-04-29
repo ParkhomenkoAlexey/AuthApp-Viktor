6f09f8e224b45168fee69ba982a44b02ebde9e11
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-04-29 20:32:07 +0500

    Initial Commit

---
+++ AuthApp/SignUpViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SignUpViewController.swift
+//  AuthApp
+//
+//  Created by Алексей Пархоменко on 29.04.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoButton: UIButton!
+    @IBOutlet weak var firstnameTextField: UITextField!
+    @IBOutlet weak var lastnameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func photoButtonTapped(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        guard let dvc = segue.destination as? ProfileViewController else { return }
+        dvc.firstname = firstnameTextField.text
+        dvc.lastname = lastnameTextField.text
+        dvc.image = photoImageView.image
+        
+    }
+}
+
+extension SignUpViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        photoImageView.image = image
+        picker.dismiss(animated: true)
+    }
+}
